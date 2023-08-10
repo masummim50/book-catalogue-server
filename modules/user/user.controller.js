c0e5b5a0f55e7242e240e2824d5f4310b7f101c6
unknown <masummim50@gmail.com>
2023-08-10 23:58:06 +0600

    list controller fixed

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -5,7 +5,7 @@ const sendResponse = require("./../../shared/sendResponse");
 
 const getLists = async(req, res, next)=> {
     try {
-        const lists = await userModel.findOne({_id:req.user._id}).select("wishlist reading");
+        const lists = await userModel.findOne({_id:req.user._id}).select("wishlist reading").populate("wishlist._id reading._id");
         if(!lists){
             throw new ApiError(400, "error retrieving the lists")
         }
